8a27f44ebbfda11c5edb57861534a48f10275fb5
PabloBueno1 <93229280+PabloBueno1@users.noreply.github.com>
2023-09-16 19:14:20 -0400

    Model Changes

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 from numpy import loadtxt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
-import streamlit as st
+
+# Neural Network
+
+def trainNetwork():
+    trainingData = loadtxt('trunc_data.csv', delimiter=',')
+
+    inData = trainingData[:,0:8]
+    outData = trainingData[:,8]
+
+    model = Sequential()
+
+    model.add(Dense(12, input_shape=(8,), activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    model.fit(inData, outData, epochs=5, batch_size=3,verbose=0)
+
+    _, accuracy = model.evaluate(inData, outData)
+    print('Accuracy: %.2f' % (accuracy*100))
+
+    predictData = loadtxt("test.csv", delimiter=',')
+    predictRow = predictData[0:8]
+
+    predictions = model.predict(inData)
+    
+    print(predictions[0])
+
 
  # UI Builder 
 
@@ -46,49 +77,40 @@ with open('style.css') as f:
         <style>
         """
 st.markdown(project_background, unsafe_allow_html=True)
+
+# Array to make CSV
+values = []
        
 project_title = st.markdown(":green[Sustainability] Calculator :earth_americas:")
       
-RV_title = st.text_input('Revenue Growth')
+RV_title = st.number_input('Revenue Growth')
 
-EB_title = st.text_input("EBITDA[*](https://www.bdc.ca/en/articles-tools/entrepreneur-toolkit/templates-business-guides/glossary/ebitda#:~:text=EBITDA%20is%20short%20for%20earnings,and%20ability%20to%20generate%20cash.)")
+EB_title = st.number_input("EBITDA[*](https://www.bdc.ca/en/articles-tools/entrepreneur-toolkit/templates-business-guides/glossary/ebitda#:~:text=EBITDA%20is%20short%20for%20earnings,and%20ability%20to%20generate%20cash.)")
 
-NE_title = st.text_input('Number of Employees')
+NE_title = st.number_input('Number of Employees')
 
-Weight_title = st.text_input('Weight %')
+Weight_title = st.number_input('Weight %')
 
-EV_title = st.text_input('Enviornmental Score')
+EV_title = st.number_input('Enviornmental Score')
 
-GoV_title = st.text_input('Government Score')
+GoV_title = st.number_input('Government Score')
 
-SS_title = st.text_input('Social Score')
+SS_title = st.number_input('Social Score')
 
-ControS_title = st.text_input('Controversial Score')
+ControS_title = st.number_input('Controversial Score')
 
-
-# Neural Network
-
-trainingData = loadtxt('trunc_data.csv', delimiter=',')
-
-inData = trainingData[:,0:8]
-outData = trainingData[:,8]
-
-model = Sequential()
-
-model.add(Dense(12, input_shape=(8,), activation='relu'))
-model.add(Dense(8, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-model.fit(inData, outData, epochs=5, batch_size=3,verbose=0)
-
-_, accuracy = model.evaluate(inData, outData)
-print('Accuracy: %.2f' % (accuracy*100))
-
-predictions = (model.predict(inData) > 0.5).astype(int)
-for i in range(3):
- print('%s => %d (expected %d)' % (inData[i].tolist(), predictions[i], outData[i]))
-
+SubmitBut = st.button("Calculate", type="primary")
+if SubmitBut:
+    values.append(RV_title)
+    values.append(EB_title)
+    values.append(NE_title)
+    values.append(Weight_title)
+    values.append(EV_title)
+    values.append(GoV_title)
+    values.append(SS_title)
+    values.append(ControS_title)
+    arr = np.asarray(values)
+    arr.tofile("test.csv",sep= ',')
+    trainNetwork()
 
 
